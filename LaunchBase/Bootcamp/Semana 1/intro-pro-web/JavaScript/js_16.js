08fd00f78b7f0374849d58e8a7144d00bf50775b
Igor Martins <igormaceio.al@gmail.com>
2020-10-24 23:25:42 -0300

    Deleted
    Boas Práticas!

--- LaunchBase/Bootcamp/Semana 1/intro-pro-web/JavaScript/js_16.js
+++
@@ -1,8 +0,0 @@
-// Aula - Escopos
-
-//https://imasters.com.br/desenvolvimento/escopos-em-javascript
-
-//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/array
-
-//Isso é algo que se aprende e se usa no dia a dia.
-
--- LaunchBase/Bootcamp/Semana 1/intro-pro-web/JavaScript/js_18.js
+++
@@ -1,5 +0,0 @@
-// Aula - Organizar, Padronizar e Escrita
-
-// Boas Práticas
-
-//https://www.devmedia.com.br/boas-praticas-de-programacao/21137
